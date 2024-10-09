f3b4bdd5c8b00753347e5fc6d5b164e55ae44035
Febyan Valentino <vfebyan@gmail.com>
2024-10-09 21:16:56 +0700

    logo

--- resources/views/pages/public.blade.php
+++ resources/views/pages/public.blade.php
@@ -1,17 +1,17 @@
 <x-splade-form method="POST" action="#">
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/301692/login.svg" alt="Workflow">
-            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-                Layanan Informasi
-            </h2>
-        </div>
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 relative">
 
-        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <img class="absolute top-0 left-0 h-20 w-auto m-4" src="https://www.svgrepo.com/show/301692/login.svg" alt="Login Icon">
+                <img class="absolute top-0 right-0 h-20 w-auto m-4" src="{{ asset('img/auth/logo.png') }}" alt="Logo SMP">
 
+                <h2 class="text-center text-3xl leading-9 font-extrabold text-gray-900 mt-12">
+                    Layanan Informasi
+                </h2>
+                
                 <!-- Dropdown untuk Pilih Guru BK -->
-                <div>
+                <div class="mt-8">
                     <x-splade-select name="teacher" label="Pilih Guru BK" placeholder="-- Pilih Guru BK --" :options="$teachers" />
                 </div>
 
