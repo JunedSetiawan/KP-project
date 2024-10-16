d0a792564e14025bd6d6779bb07b665a0929b94c
zarozhayko <zarozhayko@gmail.com>
2024-10-16 03:10:09 +0000

    update: rbac fix on route

--- routes/admin/informationservice.php
+++ routes/admin/informationservice.php
@@ -9,7 +9,7 @@ Route::get('/information-service/public', [InformationServiceController::class, 
 Route::get('/load/student/{classroom}', [InformationServiceController::class, 'loadStudent'])->name('informationservice.load.student');
 Route::get('/informationservice/create', [InformationServiceController::class, 'create'])->name('informationservice.create');
 Route::post('/informationservice/store', [InformationServiceController::class, 'store'])->name('informationservice.store');
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['can:manage-student','auth']], function () {
     Route::get('/informationservice', [InformationServiceController::class, 'index'])->name('informationservice.index');
     Route::delete('/informationservice/{classes}', [InformationServiceController::class, 'destroy'])->name('informationservice.destroy');
 });
